836eba2dc90926631ee4d32b1efdc320d892047f
Angelos2024 <skyblackroan@gmail.com>
2025-02-11 14:31:29 -0600

    Update scriptparalelo.js

--- js/scriptparalelo.js
+++ js/scriptparalelo.js
@@ -1,5 +1,9 @@
 
 
+
+
+
+
   // Definimos los libros disponibles para la versión RVR1960
   const versionesBiblia = {
     RVR1960: {
@@ -32,7 +36,74 @@
       "Apocalipsis": "https://raw.githubusercontent.com/xtiam57/church-utils/refs/heads/main/dist/biblia/apocalipsis.json"
 
     },
-    Americas: {},
+    Btx4: {
+      "Génesis": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/apocalipsis.json",
+      "Exodo": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/exodo.json",
+      "Levitico": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/levitico.json",
+      "Numeros": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/numeros.json",
+      "Deuteronomios": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/deuteronomios.json",
+      "Josue": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/josue.json",
+      "Jueces": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/jueces.json",
+      "Rut": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/rut.json",
+      "1Samuel": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/1samuel.json",
+      "2Samuel": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/2samuel.json",
+      "1Reyes": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/1reyes.json",
+      "2Reyes": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/2reyes.json",
+      "Abdias": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/abdias.json",
+      "Joel": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/joel.json",
+      "Jonas": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/jonas.json",
+      "Amos": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/amos.json",
+      "Miqueas": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/miqueas.json",
+      "Oseas": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/oseas.json",
+      "Nahum": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/nahum.json",
+      "Isaias": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/isaias.json",
+      "Jeremias": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/jeremias.json",
+      "Lamentaciones": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/lamentacioes.json",
+      "Sofonias": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/sofonias.json",
+      "Habacuc": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/habacuc.json",
+      "Ezequiel": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/ezequiel.json",
+      "Daniel": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/daniel.json",
+      "Ester": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/ester.json",
+      "1Cronicas": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/1cronicas.json",
+      "2Cronicas": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/2cronicas.json",
+      "Esdras": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/esdras.json",
+      "Nehemias": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/nehemias.json",
+      "Hageo": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/hageo.json",
+      "Zacarias": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/zacarias.json",
+      "Malaquias": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/malaquias.json",
+      "Job": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/job.json",
+      "Salmos": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/salmos.json",
+      "Proverbios": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/proverbios.json",
+      "Eclesiastes": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/eclesiastes.json",
+      "Cantares": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/cantares.json",
+      "Juan": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/juan.json",
+      "Mateo": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/mateo.json",
+      "Marcos": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/marcos.json",
+      "Lucas": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/lucas.json",
+      "Hechos": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/hechos.json",
+      "Jacobo": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/jacobo.json",
+      "1Pedro": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/1pedro.json",
+      "2Pedro": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/2pedro.json",
+      "Judas": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/judas.json",
+      "1Juan": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/1juan.json",
+      "2Juan": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/2juan.json",
+      "3Juan": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/3juan.json",
+      "Galatas": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/galatas.json",
+      "1Tesalonicenses": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/1tesalonicenses.json",
+      "2Tesalonicenses": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/2tesalonicenses.json",
+      "1Corintios": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/1corintios.json",
+      "2Corintios": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/2corintio.json",
+      "Romanos": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/romanos.json",
+      "Efesios": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/efesios.json",
+      "Filipenses": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/filipenses.json",
+      "Colosenses": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/colosenses.json",
+      "Hebreos": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/hebreos.json",
+      "Filemon": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/filemon.json",
+      "1Timoteo": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/1timoteo.json",
+      "2Timoteo": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/2timoteo.json",
+      "Tito": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/tito.json",
+      "Apocalipsis": "https://raw.githubusercontent.com/Angelos2024/torah-Yehoshua/refs/heads/main/BTX4/apocalipis.json"
+          },
     Jubileos: {}
   };
 
@@ -41,6 +112,11 @@
 
 
  
+ function cambiarVersion() {
+  currentVersion = document.getElementById("versionSelector").value;
+  cargarLibros(versionesBiblia[currentVersion]);
+}
+
 
   function closesidebar2() {
     const sidebar2 = document.getElementById("sidebar2");
@@ -57,11 +133,11 @@ document.addEventListener("DOMContentLoaded", () => {
 async function sincronizarVersion() {
     // Verificar si hay un libro y capítulo cargado en la versión hebrea
     if (!currentBook || !currentChapter) {
-        alert("Navega a un capítulo en la versión hebrea antes de sincronizar.");
+        alert("Navega a un capítulo en la versión principal antes de sincronizar.");
         return;
     }
 
-    console.log(`Sincronizando desde la Biblia hebrea: ${currentBook} ${currentChapter}`);
+    console.log(`Sincronizando desde la Biblia principal: ${currentBook} ${currentChapter}`);
 
     // Obtener selectores de la Biblia paralela
     let bookSelector = document.getElementById("bookSelector");
