71e34f48732745130ce720223f8ec25cf1ad56ae
Angelos2024 <skyblackroan@gmail.com>
2025-02-11 18:01:52 -0600

    Add files via upload

---
+++ na28/js/comentarios.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const comentarios = {
+        "verso2": "na28/2ts1/verso2.html",
+        "verso4": "na28/2ts1/verso4.html",
+        "verso8": "na28/2ts1/verso8.html",
+       
+        // Agrega más comentarios aquí
+    };
+
+    const contenedorComentarios = document.getElementById("contenidoSidebar");
+
+    Object.entries(comentarios).forEach(([id, url]) => {
+        let div = document.createElement("div");
+        div.id = id;
+        div.classList.add("seccionSidebar");
+        div.style.display = "none";
+        div.innerHTML = `<object type="text/html" data="${url}" width="100%" height="300px"></object>`;
+        
+        contenedorComentarios.appendChild(div);
+    });
+
+});
