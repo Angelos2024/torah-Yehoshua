f016cdef28fc0edaa52000592d2f754340e7a80e
Angelos2024 <skyblackroan@gmail.com>
2025-03-14 15:55:55 -0600

    Update fu8.js

--- na28/js/fu8.js
+++ na28/js/fu8.js
@@ -174,7 +174,7 @@ const na28Map = {
         "hebreos2_11": "na28/2ts1/hebreos2_11.html",
         "hebreos2_14": "na28/2ts1/hebreos2_14.html",
         "hebreos2_17": "na28/2ts1/hebreos2_17.html",
-         "Gálatas1_1": "na28/2ts1/galatas1_1.html",
+         "Galatas1_1": "na28/2ts1/galatas1_1.html",
         "galatas1_3": "na28/2ts1/galatas1_3.html",
         "galatas1_4": "na28/2ts1/galatas1_4.html",
         "galatas1_6": "na28/2ts1/galatas1_6.html",
