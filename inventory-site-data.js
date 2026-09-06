86672196fa81bf2ba67bfe42d9088e244df5050f
Angelos2024 <skyblackroan@gmail.com>
2026-09-06 15:07:43 -0600

    Actualizar existencias del sitio (6/9/2026, 3:07:43 p.m.)

--- inventory-site-data.js
+++ inventory-site-data.js
@@ -1,5 +1,5 @@
 window.INVENTORY_SITE_DATA = {
-  "savedAt": "2026-09-06T20:06:31.093Z",
+  "savedAt": "2026-09-06T21:07:33.085Z",
   "source": "zapatos",
   "data": {
     "nextId": 130,
@@ -28,15 +28,14 @@ window.INVENTORY_SITE_DATA = {
         "audienceGroup": "caballero",
         "productGroup": "calzado-vaquero",
         "model": "Nicadabra total black",
-        "size": "26 x2, 27, 28, 29 x2, 30",
+        "size": "26 x2, 28, 29 x2, 30",
         "sizeDetails": {
           "26": 2,
-          "27": 1,
           "28": 1,
           "29": 2,
           "30": 1
         },
-        "quantity": 7,
+        "quantity": 6,
         "minimum": 3,
         "cost": 500,
         "salePrice": 840
